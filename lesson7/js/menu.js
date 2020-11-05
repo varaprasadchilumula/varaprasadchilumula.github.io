57dee44849365e724b34452b24d3d86fb938ab04
varaprasadlds <varaprasadlds@gmail.com>
2020-11-05 23:15:20 +0530

    commit

---
+++ lesson7/js/menu.js
@@ -0,0 +1,6 @@
+const hambutton = document.querySelector('.ham');
+const mainnav = document.querySelector('.navigation')
+
+hambutton.addEventListener('click', () => {
+    mainnav.classList.toggle('responsive')
+}, false);
